52c7b70a3c3ed3a3106aa1b422e7d7eb4559909b
kranik <optimalx@mail.ru>
2026-06-27 18:02:10 +0500

    feat: add create dir for downloaded images method

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import argparse
 import asyncio
+import os
 
 DEFAULT_DIR = "downloaded_images"
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Download images from a list of URLs.")
-    parser.add_argument("url", nargs=None, help="List of URLs to download images from.")
+    parser.add_argument("url", nargs=None, help="URL to download images from.")
     parser.add_argument(
         "--dir", default=DEFAULT_DIR, help="Directory to save downloaded images."
     )
     return parser.parse_args()
 
 
+def ensure_dir_exists(dir: str) -> None:
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+
 async def main():
     args = parse_args()
 
-    print(args)  # TODO: Implement the download logic here
+    ensure_dir_exists(args.dir)
 
 
 if __name__ == "__main__":
