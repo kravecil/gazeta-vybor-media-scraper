f0f9a63ed430ff35df67d7f56abbb6a557797e79
kranik <optimalx@mail.ru>
2026-06-27 17:58:14 +0500

    feat: add argument parser

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+import argparse
 import asyncio
+
+DEFAULT_DIR = "downloaded_images"
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Download images from a list of URLs.")
+    parser.add_argument("url", nargs=None, help="List of URLs to download images from.")
+    parser.add_argument(
+        "--dir", default=DEFAULT_DIR, help="Directory to save downloaded images."
+    )
+    return parser.parse_args()
 
 
 async def main():
-    pass
+    args = parse_args()
+
+    print(args)  # TODO: Implement the download logic here
 
 
 if __name__ == "__main__":
